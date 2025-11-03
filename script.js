e21de34c05da1a0582ed35cf0296b14278bb7601
mahaltrupti <truptimahal2004@gmail.com>
2025-11-04 00:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const buttonsContainer = document.getElementById('buttons');
+const sounds = document.querySelectorAll('.btn');
+let currentAudio = null;
+
+sounds.forEach(button => {
+  button.addEventListener('click', () => {
+    const soundFile = button.getAttribute('data-sound');
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+    currentAudio = new Audio(`sounds/${soundFile}`);
+    currentAudio.play();
+  });
+});
+
+const stopButton = document.querySelector('.stop');
+stopButton.addEventListener('click', () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
+
